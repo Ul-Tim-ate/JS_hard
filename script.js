f774ec1e2e16d4521fe282d89efd81444c071b11
Tim <timohator1@mail.ru>
2020-10-11 14:35:01 +0300

    Do task for lesson02

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-alert('Pa Pa Power');
-console.log('Dead Men Bones');
-alert('Hello everybudy');
-let asds;
+let money = 250000;
+let income = 'репетиторство';
+let addExpenses = 'Оплата жилья, продукты, интернет';
+let deposit = true;
+let mission = 1000000;
+let period = 12;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен', period, 'месяцев');
+console.log('Цель заработать', mission, 'тенге');
+addExpenses.toLocaleLowerCase();
+console.log(addExpenses.split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
