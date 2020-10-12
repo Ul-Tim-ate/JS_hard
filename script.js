d583bef35f47392da453373c0fd20e8e3f3ba956
Tim <timohator1@mail.ru>
2020-10-12 12:13:13 +0300

    Do hard task for lesson03

--- script.js
+++ script.js
@@ -1,47 +1,25 @@
 'use strict';
-let money = 250000;
-let income = 'репетиторство';
-let addExpenses = 'Оплата жилья, продукты, интернет';
-let deposit = true;
-let mission = 1000000;
-let period = 12;
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log('Период равен', period, 'месяцев');
-console.log('Цель заработать', mission, 'тенге');
-addExpenses.toLocaleLowerCase();
-console.log(addExpenses.split(', '));
-
-let budgetDay = money / 30;
-console.log(budgetDay);
-
-
-
-money = prompt('Ваш месячный доход?');
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-deposit = confirm('Есть ли у вас депозит в банке?');
-let expenses1, expenses2, amount1, amount2;
-expenses1 = prompt('Введите обязательную статью расходов');
-amount1 = prompt('Во сколько это обойдется?');
-expenses2 = prompt('Введите обязательную статью расходов');
-amount2 = prompt('Во сколько это обойдется?');
-let budgetMounth = money - amount1 - amount2;
-console.log('Бюджет на месяц:', budgetMounth);
-
-console.log('До вашей цели:', Math.ceil(mission / budgetMounth), 'месяцев');
-
-budgetDay = budgetMounth / 30;
-console.log('Бюджет на день:', Math.floor(budgetDay));
-
-if (budgetDay >= 1200) {
-	console.log('У вас высокий уровень дохода');
-}else if (budgetDay >= 600) {
-	console.log('У вас средний уровень дохода');
-}else if (budgetDay >= 0) {
-	console.log('У вас низкий уровень дохода');
-} else {
-	console.log('Что-то пошло не так');
+let lang = document.documentElement.lang;
+if (lang === 'ru') {
+	console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+}
+if (lang === 'en') {
+	console.log('Monday, tuesday, wednesday, thursday, friday, saturday, sunday');
 }
 
+switch (lang) {
+	case 'ru':
+		console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+		break;
+	case 'en':
+		console.log('Monday, tuesday, wednesday, thursday, friday, saturday, sunday');
+		break;
+}
+
+let days = ['en', 'ru'];
+days[0] = ['Monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+days[1] = ['Понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+lang === 'en' ? console.log(days[0]) : console.log(days[1]);
+
+let namePerson = 'чмо';
+namePerson === 'Артём' ? console.log('Директор') : namePerson === 'Максим' ? console.log('Преподаватель') : console.log('Студент');
