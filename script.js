aa7c215003e165edd2b15f1a317e76a2b4e29848
Tim <timohator1@mail.ru>
2020-10-15 00:15:57 +0300

    Do hard task for lesson05

--- script.js
+++ script.js
@@ -1,49 +1,20 @@
 'use strict';
-let money = +prompt('Ваш месячный доход?');
-let income = 'репетиторство';
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-let deposit = confirm('Есть ли у вас депозит в банке?');
-let mission = 1000;
-let period = 12;
-let expenses1 = prompt('Введите обязательную статью расходов'),
-	 amount1 = +prompt('Во сколько это обойдется?'),
-	 expenses2 = prompt('Введите обязательную статью расходов'),
-	 amount2 = +prompt('Во сколько это обойдется?');
-let accumulatedMonth;
-let budgetDay;
-//объявление всех переменных
-function showTypeOf(data) {
-	console.log(data, typeof data);
-}
-function getExpensesMonth() { 
-	return amount1 + amount2;
-}
-function getAccumulatedMonth() {
-	return money - getExpensesMonth();
-}
-function getTargetMonth() {
-	return Math.ceil(mission / accumulatedMonth);
-}
-function getStatusIncome() { 
-		if (budgetDay >= 1200) {
-		return 'У вас высокий уровень дохода';
-	}else if (budgetDay >= 600) {
-		return 'У вас средний уровень дохода';
-	}else if (budgetDay >= 0) {
-		return 'У вас низкий уровень дохода';
-	} else {
-		return 'Что-то пошло не так';
+let arr = ["294034", "36452", "812523", "533421", "4354213", "235436356", "912134"];
+for (let i = 0; i < arr.length; i++) {
+	const element = arr[i];
+	if (arr[i][0] === "2" || arr[i][0] === "4" ) {
+		console.log(arr[i]);
 	}
 }
-//все функции
-accumulatedMonth = getAccumulatedMonth();
-budgetDay = Math.floor(accumulatedMonth / 30);
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-addExpenses.toLocaleLowerCase();
-console.log(addExpenses.split(', '));
-console.log(getExpensesMonth());
-console.log(getTargetMonth());
-console.log('Бюджет на день:', Math.floor(budgetDay));
-console.log(getStatusIncome());
+for (let i = 2; i < 100; i++) {
+		let del;
+		for (let j = 1; j <= i; j++) {
+			if (i % j === 0 && j !== 1) {
+				del = j;
+				break;
+			}
+		}
+		if (del === i) {
+			console.log('Делители числа', del, ': 1 и', i);
+		}
+	}
