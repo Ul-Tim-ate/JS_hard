9c77c55098ff7d1cdf8cb48f09ccabe76f8b4c18
Tim <timohator1@mail.ru>
2020-10-15 19:21:15 +0300

    Do hard task for lesson06

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ function isNumber(n) {
 	return !isNaN(parseFloat(n)) && isFinite(n);
 }
 function start() {
+	let count = 10;
 	let flag = confirm('Угадай число от 1 до 100');
-	let number = 25;
+	let number = Math.floor(Math.random() * 100) + 1;
 	function guess() {
 		if (!flag) {
 			alert('Игра закончилась так и начавшись!!!');
 			return;
+		}
+		if (count === 0) {
+			if (confirm('You lose, you get nothing!!! Хотите сыграть еще?')) {
+				let temp = start();
+				temp();
+			}
+			else { return; }
 		}
 		let answer = prompt('Введи число');
 		if (answer === null) {
@@ -20,16 +28,19 @@ function start() {
 		}
 		answer = parseFloat(answer);
 		if (answer > number) {
-			alert('Загаданное число меньше');
+			alert('Загаданное число меньше, у вас осталось попыток ' + --count);
 			guess();
 		}
 		if (answer < number) {
-			alert('Загаданное число больше');
+			alert('Загаданное число больше, у вас осталось попыток ' + --count);
 			guess();
 		}
 		if (answer === number) {
-			alert('Поздравляю, Вы угадали!!!');
-			return;
+			if (confirm('Поздравляю, Вы угадали!!! Хотите сыграть еще?')) {
+				let temp = start();
+				temp();
+			}
+			else { return; }
 		}
 	}
 	return guess;
