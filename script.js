478a37076e883508e132a07ca3941f32d6d75711
Tim <timohator1@mail.ru>
2020-10-17 09:38:43 +0300

    Do lesson06

--- script.js
+++ script.js
@@ -1,57 +1,81 @@
 'use strict';
+let money;
 function isNumber(n) {
 	return !isNaN(parseFloat(n)) && isFinite(n);
 }
 function start() {
-	let count = 10;
-	let flag = confirm('Угадай число от 1 до 100');
-	let number = Math.floor(Math.random() * 100) + 1;
-	function guess() {
-		if (!flag) {
-			alert('Игра закончилась так и начавшись!!!');
-			return;
-		}
-		if (count === 0) {
-			if (confirm('You lose, you get nothing!!! Хотите сыграть еще?')) {
-				let temp = start();
-				temp();
+	do {
+		money = prompt('Ваш месячный доход?');
+	} while (!isNumber(money));
+}
+start();
+let appData = {
+	income: {},
+	addIncome: [],
+	expenses: {},
+	addExpenses: [],
+	deposit: false,
+	mission: 50000,
+	peroid: 6,
+	budget: money,
+	budgetDay: 0,
+	budgetMounth: 0,
+	expensesMonth: 0,
+	asking: function () {
+		let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+		appData.addExpenses = addExpenses.toLocaleLowerCase().split(', ');
+		appData.deposit = confirm('Есть ли у вас депозит в банке?');
+		let sum = 0;
+		let temp;
+		for (let i = 0; i < 2; i++) {
+			let costs = prompt('Введите обязательную статью расходов');
+			do {
+				temp = +prompt('Во сколько это обойдется?');
+			} while (!isNumber(temp));
+			appData.expenses[costs] = temp; 
+			sum += temp;
 			}
-			else {
-				alert('Игра окончена');
-				return;
-			}
+	},
+	getExpensesMonth: function () { 
+		for (let i  in  appData.expenses) {
+			appData.expensesMonth += appData.expenses[i]; 
 		}
-		let answer = prompt('Введи число');
-		if (answer === null) {
-			alert('Игра окончена');
-			return;
-		}
-		if (!isNumber(answer)) {
-			guess();
-		}
-		answer = parseFloat(answer);
-		if (answer > number) {
-			alert('Загаданное число меньше, у вас осталось попыток ' + --count);
-			guess();
-		}
-		if (answer < number) {
-			alert('Загаданное число больше, у вас осталось попыток ' + --count);
-			guess();
-		}
-		if (answer === number) {
-			if (confirm('Поздравляю, Вы угадали!!! Хотите сыграть еще?')) {
-				let temp = start();
-				temp();
-			}
-			else {
-				alert('Игра окончена');
-				return;
-			}
+	},
+	getBudget: function () {
+		appData.budgetMounth = appData.budget - appData.expensesMonth;
+		appData.budgetDay = Math.floor(appData.budgetMounth / 30);
+		return appData.budgetMounth;
+	},
+	getTargetMonth: function () {
+		return Math.ceil(appData.mission / appData.budgetMounth);
+	},
+	getStatusIncome: function () { 
+		if (appData.budgetDay >= 1200) {
+			return 'У вас высокий уровень дохода';
+		}else if (appData.budgetDay >= 600) {
+			return 'У вас средний уровень дохода';
+		}else if (appData.budgetDay >= 0) {
+			return 'У вас низкий уровень дохода';
+		} else {
+			return 'Что-то пошло не так';
 		}
 	}
-	return guess;
+};
+
+appData.asking();
+appData.getExpensesMonth();
+appData.getBudget();
+console.log('Расходы за месяц:', appData.expensesMonth);
+if (appData.getTargetMonth() > 0) {
+	console.log('Цель будет достигнута через', appData.getTargetMonth(), 'месяца');
+} else {
+	console.log('Цель не будет достигнута');
 }
+console.log(appData.getStatusIncome());
 
-
-let a = start();
-a();
+console.log('Наша программа включает в себя данные:');
+for (const key in appData) {
+	if (appData.hasOwnProperty(key)) {
+		console.log(key, appData[key]);
+	}
+}
